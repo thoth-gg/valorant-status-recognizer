4f0e6068c38bb332219abdc1c167af16341a3315
GOTO Ritsuki <develop@mizucoffee.net>
2024-08-14 12:03:27 +0900

    feat: frame rate

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import cv2
 import numpy as np
+import requests
+from time import time, sleep
 
 camera = cv2.VideoCapture(2)
 camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 
 result = np.zeros((10, 10), np.uint8)
+latest = np.zeros((10), np.uint8)
 
 def get_hp(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -28,7 +31,11 @@ def red_image(frame):
     maskBGR = cv2.inRange(frame,minBGR,maxBGR)
     return cv2.bitwise_and(frame, frame, mask = maskBGR)
 
+framerate = 5
+oneFrameTime = 1 / framerate
+
 while True:
+    startAt = time()
     ret, frame = camera.read(0)
     extracted_frame = cv2.add(white_image(frame), red_image(frame))
 
@@ -44,19 +51,15 @@ while True:
 
     median = np.median(result, axis=1)
 
-    print(median)
+    if((latest != median).any()):
+        print(median)
+        latest = median
+        data = {
+            "hp": median.tolist()
+        }
+        requests.post('http://localhost:3000/hp', json=data)
 
-    # y = 85
+    endAt = time()
 
-    # for i in range(0, 5):
-    #     cv2.line(extracted_frame, (446 + i * 66, y), (446 + round(40 * median[i]) + i * 66, y), (255, 255, 0), 2, cv2.LINE_4)
-    #     cv2.putText(extracted_frame, str(round(median[i] * 100)), (446 + i * 66 + 4, y - 60), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255,255,255))
-
-    # for i in range(0, 5):
-    #     cv2.line(extracted_frame, (1171 + i * 66, y), (1171 + round(40 * median[i + 5]) + i * 66, y), (255, 255, 0), 2, cv2.LINE_4)
-    #     cv2.putText(extracted_frame, str(round(median[i + 5] * 100)), (1171 + i * 66 + 4, y - 60), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255,255,255))
- 
-    # cv2.imshow('camera', extracted_frame)
-    
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+    if endAt - startAt < oneFrameTime:
+        sleep(oneFrameTime - (endAt - startAt))
